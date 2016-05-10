ff2ea871c6560bf26d72c0de7bb8fe196b58422f
Patrick Camacho <patrick@daftdevelopers.com>
2016-05-09 18:24:35 -0700

    init project

---
+++ components/button/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+import css from './styles.css';
+
+const Button = props => {
+  const { children, className, ...rest } = props;
+
+  return (
+    <button
+      className={classnames(css.button, className)}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+export default Button;
---
+++ components/logo/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import React from 'react';
+import css from './styles.css';
+
+const Logo = () => (
+  <div className={css.logo}>
+    <span className={css.glass}>glass</span>
+    <span className={css.falcon}>falcon</span>
+    <span className={css.io}>.io</span>
+  </div>
+);
+
+export default Logo;
