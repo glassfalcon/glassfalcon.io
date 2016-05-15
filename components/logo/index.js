a44f6c824035ed9d679e7ba2303a8f092f204baa
Patrick Camacho <patrick@daftdevelopers.com>
2016-05-14 20:47:26 -0400

    added logo

--- components/logo/index.js
+++ components/logo/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable max-len */
 import React from 'react';
+
 import css from './styles.css';
 
 const Logo = () => (
   <div className={css.logo}>
-    <span className={css.glass}>glass</span>
-    <span className={css.falcon}>falcon</span>
-    <span className={css.io}>.io</span>
+    <div className={css.falcon} />
+    glassfalcon
   </div>
 );
 
